5278498c6a43324656e056269f53fbf02bfc5c9f
Ronihe <herongrong2011@gmail.com>
2018-10-30 03:58:29 -0700

    3rd

--- script.js
+++ script.js
@@ -10,20 +10,33 @@
 
 // let start = {
 
-//   pics : [],
-
-// // pass in the pics, and make the game page;
+//   pics : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
 
 //   shuffleCards : function(){
+//     let imgs = document.getElementsByClassName("");
+
+//     imgs.forEach(function(val, idx){
+//       val
+
+//     })
+
+
 //     this.pics
 
 //   }
 
-// };
+// // };
 
 // let play = {
 
+
+//   flipped: [],
+
+//   flipped
+
 //   flip: function(){
+
+
 
 //   },
 
@@ -32,11 +45,93 @@
 
 //   },
 
+//   ClickOnce: function(){
 
 
+//   },
+
+//   check: function(){
+
+//   }
 // }
 
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
+
+
+
+let flipped = 0;
+let totalClick = 0;
+let cards = document.querySelectorAll(".card");
+
+// max is 2
+let flipCount = 0;
+let subFlipped = [];
+
+
+
+function start (){
+  cards.forEach(card => {
+    card.addEventListener( "click", function(){
+      // toggleCard(card);
+      flip(card);
+    });  
+  }); 
+}
+
+function toggleCard(card){
+  card.classList.toggle("is-flipped");
+}
+
+function win(){
+  if (flipped === 24){
+      alert("You Win!")
+  }
+}
+
+function flip(card){
+  
+  if(flipCount === 0){
+    if(card.classList.contains("is-flipped")){
+      card.removeEventListener("click", toggleCard);
+    }else{
+    toggleCard(card);
+    flipCount++;
+    totalClick++;
+    subFlipped.push(card);
+    viewTotal();}
+  }else if(flipCount === 1){
+    if(card.classList.contains("is-flipped")){
+      card.removeEventListener("click", toggleCard);
+    }else{
+      toggleCard(card);
+      subFlipped.push(card);
+      totalClick++;
+      viewTotal();
+      check();
+      flipCount = 0;
+      subFlipped = [];
+    }
+  }
+
+  console.log(flipCount, subFlipped);
+  
+  
+  }
+  
+  function check(){
+    console.log(subFlipped[0].getElementsByTagName("img")[0].src === subFlipped[1].getElementsByTagName("img")[0].src);
+    if(subFlipped[0].getElementsByTagName("img")[0].src === subFlipped[1].getElementsByTagName("img")[0].src){
+    flipped += 2;
+    console.log(flipped);
+    win();
+   }else{
+
+  window.setTimeout(subFlipped.forEach(function(el){
+    toggleCard(el);
+  }),1000);
+  }
+}
+
+
+  function viewTotal(){
+    document.getElementById("totalClicks").innerText=totalClick;
+  }
