7756e6cbd56886e73d7c14d635f40b475b879b65
Ronihe <herongrong2011@gmail.com>
2018-10-30 18:48:20 -0700

    semi-final

--- script.js
+++ script.js
@@ -1,94 +1,66 @@
-//start the game and Store the lowest-scoring game in local storage
-//  randomly assign the pics to different blocks, using math.random
 
-// set the click = 0, every click click++, and set the card as "clicked", user can not click it again, change the class of the card when the card the flipped
-//set n = 0, flip the card on click, n++, when n===2, if the same, stay flipped, if not stay flipped for one second and flip;
-
-// loop until all flipped
-
-
-
-// let start = {
-
-//   pics : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-
-//   shuffleCards : function(){
-//     let imgs = document.getElementsByClassName("");
-
-//     imgs.forEach(function(val, idx){
-//       val
-
-//     })
-
-
-//     this.pics
-
-//   }
-
-// // };
-
-// let play = {
-
-
-//   flipped: [],
-
-//   flipped
-
-//   flip: function(){
-
-
-
-//   },
-
-//   match: function(){
-
-
-//   },
-
-//   ClickOnce: function(){
-
-
-//   },
-
-//   check: function(){
-
-//   }
-// }
-
-
-
-
-let flipped = 0;
-let totalClick = 0;
+let flipped = 0; // if the flipped is 24, the user won!
+let totalClick = 0; // the total clicks, can be saved in the local storage
 let cards = document.querySelectorAll(".card");
+let flipCount = 0; // max is 2
+let subFlipped = [];// two cards to match
 
-// max is 2
-let flipCount = 0;
-let subFlipped = [];
+// pics in the game board
+let pics = ["pics/1.gif", "pics/2.gif", "pics/3.gif", "pics/4.gif", "pics/5.jpg", "pics/6.gif", "pics/1.gif", "pics/2.gif", "pics/3.gif", "pics/4.gif", "pics/5.jpg", "pics/6.gif", "pics/1.gif", "pics/2.gif", "pics/3.gif", "pics/4.gif", "pics/5.jpg", "pics/6.gif", "pics/1.gif", "pics/2.gif", "pics/3.gif", "pics/4.gif", "pics/5.jpg", "pics/6.gif"];
 
+//shuffle the cards
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
 
+// locate the shuffled cards to different blocks
+function locatePic(){
+  let shuffledCards = shuffle(pics);
+  cards.forEach((card, idx) => {
+    card.getElementsByTagName("img")[0].src = shuffledCards[idx];
+    });
+}
 
-function start (){
+// click the start key to make the game start
+function start(){
+  flipped = 0;
+  totalClick = 0;
+  flipCount = 0;
+  subFlipped = [];
+  locatePic();
+  viewTotal();
+  location.href="#page2";
   cards.forEach(card => {
+    if(card.classList.contains("is-flipped")){
+      toggleCard(card);
+    }
     card.addEventListener( "click", function(){
-      // toggleCard(card);
+      //card.classList.remove("is-flipped");
+      // // toggleCard(card);
+      // card.
       flip(card);
     });  
   }); 
 }
 
+//flip the card
 function toggleCard(card){
   card.classList.toggle("is-flipped");
 }
 
+//Win alert!
 function win(){
   if (flipped === 24){
       alert("You Win!")
   }
 }
 
-function flip(card){
-  
+//flip the card.
+function flip(card){  
   if(flipCount === 0){
     if(card.classList.contains("is-flipped")){
       card.removeEventListener("click", toggleCard);
@@ -97,7 +69,7 @@ function flip(card){
     flipCount++;
     totalClick++;
     subFlipped.push(card);
-    viewTotal();}
+    viewTotal()}
   }else if(flipCount === 1){
     if(card.classList.contains("is-flipped")){
       card.removeEventListener("click", toggleCard);
@@ -107,31 +79,37 @@ function flip(card){
       totalClick++;
       viewTotal();
       check();
-      flipCount = 0;
-      subFlipped = [];
     }
-  }
-
-  console.log(flipCount, subFlipped);
-  
-  
-  }
-  
-  function check(){
-    console.log(subFlipped[0].getElementsByTagName("img")[0].src === subFlipped[1].getElementsByTagName("img")[0].src);
-    if(subFlipped[0].getElementsByTagName("img")[0].src === subFlipped[1].getElementsByTagName("img")[0].src){
-    flipped += 2;
-    console.log(flipped);
-    win();
-   }else{
-
-  window.setTimeout(subFlipped.forEach(function(el){
-    toggleCard(el);
-  }),1000);
   }
 }
 
+//flip the cards back if they are matching
+  function flipCards() {
+    subFlipped.forEach(function(el){
+      toggleCard(el); 
+    });
+    flipCount = 0;
+    subFlipped = [];
+  }
 
+  // check the two cards 
+  function check(){
+    if(subFlipped[0].getElementsByTagName("img")[0].src === subFlipped[1].getElementsByTagName("img")[0].src){
+      flipped += 2;
+      win();// check if the user wins
+      flipCount = 0;
+      subFlipped = [];
+    }else{
+      console.log(subFlipped);
+      window.setTimeout(function () { flipCards() }, 1000)
+  }
+}
+
+//view the Total clicks
   function viewTotal(){
     document.getElementById("totalClicks").innerText=totalClick;
   }
+
+//local storage;
+
+
